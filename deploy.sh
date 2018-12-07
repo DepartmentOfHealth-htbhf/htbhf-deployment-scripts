8462866f359b426d96518424dec4ec62766516be
Matthew Smith <msmith@equalexperts.com>
2018-12-07 17:59:48 +0000

    HTBHFB-23 Added example usage to readme, removed java-specific deployment code

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ check_variable_is_set(){
     fi
 }
  # check necessary environment variables are set and not empty
-check_variable_is_set BIN_DIR
 check_variable_is_set APP_NAME
-check_variable_is_set APP_LOCATION
+check_variable_is_set APP_PATH
+check_variable_is_set BIN_DIR
 check_variable_is_set CF_SPACE
 check_variable_is_set CF_API
 check_variable_is_set CF_ORG
@@ -19,8 +19,8 @@ check_variable_is_set CF_USER
 check_variable_is_set CF_PASS
 check_variable_is_set CF_DOMAIN
 check_variable_is_set CF_PUBLIC_DOMAIN
-check_variable_is_set SMOKE_TESTS
 check_variable_is_set PROTOCOL
+check_variable_is_set SMOKE_TESTS
 
 /bin/bash ${BIN_DIR}/install_cf_cli.sh;
 
@@ -31,10 +31,7 @@ APP_FULL_NAME="$APP_NAME-$CF_SPACE"
 echo "Logging into cloud foundry with api:$CF_API, org:$CF_ORG, space:$CF_SPACE with user:$CF_USER"
 cf login -a ${CF_API} -u ${CF_USER} -p "${CF_PASS}" -s ${CF_SPACE} -o ${CF_ORG}
 
-echo "Deploying $APP_FULL_NAME to $CF_SPACE"
-
-APP_VERSION=`cat version.properties | grep "version" | cut -d'=' -f2`
-APP_PATH="${APP_LOCATION}/$APP_NAME-$APP_VERSION.jar"
+echo "Deploying $APP_FULL_NAME to $CF_SPACE from $APP_PATH"
 
 # if the app already exists, perform a blue green deployment, if not then a regular deployment
 if cf app ${APP_FULL_NAME} >/dev/null 2>/dev/null; then
